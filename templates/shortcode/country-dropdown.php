a4c7a355c734744f6bbb781c6b7ac31b91220022
superadona119 <dusandev119@gmail.com>
2023-08-16 16:26:59 +0200

    country select

---
+++ templates/shortcode/country-dropdown.php
@@ -0,0 +1,15 @@
+<div class="contact-form">
+
+    <div class="contact-form__item">[text* first-name placeholder "First Name"]</div>
+    <div class="contact-form__item">[text* last-name placeholder "Last Name"]</div>
+    <div class="contact-form__item">[country_select* country_select-801 id:1 exclude_countries:fo-de]</div>
+    <div class="contact-form__item">[email* email-address placeholder "Email"]</div>
+    <div class="contact-form__item">[text* zip-code placeholder "ZIP Code"]</div>
+    <div class="contact-form__item"></div>
+    <div class="contact-form__item">[text plan-purchase placeholder "When do you plan on purchasing?"]</div>
+    <div class="contact-form__item">[textarea additional-models placeholder "Questions or additional models?"]</div>
+</div>
+
+<div class="submit-button">
+    [submit "Send"]
+</div>
